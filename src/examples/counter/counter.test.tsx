f6ea8e3ac75ce26f7785d56658b905e55f06d970
nolann7 <nolann709@gmail.com>
2023-08-02 10:20:51 +0300

    using rtl

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,21 @@
+// @vitest-environment happy-dom
+
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  render(<Counter />);
+
+  const curCount = screen.getByTestId('current-count');
+  expect(curCount).toHaveTextContent(/0/);
+
+  const incrButton = screen.getByRole('button', { name: /increment/i });
+  await userEvent.click(incrButton);
+
+  expect(curCount).toHaveTextContent(/1/);
+});
