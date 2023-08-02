ce76455c8300d6ca1862db693b309e9b41b6da94
nolann7 <nolann709@gmail.com>
2023-08-02 12:25:36 +0300

    complete packing-list test

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,19 +10,38 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  const inputNewItem = screen.getByLabelText(/new item name/i);
+  expect(inputNewItem);
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', async () => {
+  render(<PackingList />);
+  const newItemButton = screen.getByRole('button', { name: /Add New Item/i });
+  const inputNewItem = screen.getByLabelText(/new item name/i);
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+  expect(inputNewItem).toHaveValue('');
+  expect(newItemButton).toBeDisabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const inputNewItem = screen.getByLabelText(/new item name/i);
+  const newItemButton = screen.getByRole('button', { name: /Add New Item/i });
+
+  await user.type(inputNewItem, 'wow');
+
+  expect(newItemButton).toBeEnabled();
+});
+
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const inputNewItem = screen.getByLabelText(/new item name/i);
+  const newItemButton = screen.getByRole('button', { name: /Add New Item/i });
+
+  await user.type(inputNewItem, 'wow');
+  await user.click(newItemButton);
+  // screen.debug();
+  expect(screen.getByLabelText('wow')).not.toBeChecked();
+});
